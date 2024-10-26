106cbb73d5650bd4f83b140bb0638e16a9b21fd0
fredgido <fredgido@gmail.com>
2024-10-26 23:12:56 +0000

    konachan migration scripts

---
+++ script/fixes/kona_1_refresh_thumbnails.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require_relative "base"
+
+asset_id = ARGV[0].to_i
+
+assets = asset_id > 0 ? MediaAsset.where("id > ?", asset_id) : MediaAsset.all
+
+puts asset_id > 0 ? "Processing assets with IDs greater than ##{asset_id}." : "No valid ID provided. Processing all assets."
+
+File.open('failed_thumbnail_files.txt', 'a') do |log_file|
+  assets.find_each do |asset|
+    next if asset.variant("720x720").open_file
+    begin
+      asset.original.open_file! do |original_file|
+        # Attempt to distribute files
+        asset.distribute_files!(original_file, variants: asset.variants.without(asset.original))
+        puts "##{asset.id} asset thumbnails regenerated"
+      end
+    rescue Errno::ENOENT => e
+      error_message = "Error: Unable to open original file for Asset ##{asset.id}: #{e.message}. Skipping regeneration."
+      puts error_message
+      log_file.puts(error_message)
+    rescue StandardError => e
+      error_message = "An unexpected error occurred while processing Asset ##{asset.id}: #{e.message}. Skipping regeneration."
+      puts error_message
+      log_file.puts(error_message)
+    end
+  end
+end
---
+++ script/fixes/kona_4_refresh_iqdb.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require_relative "base"
+
+post_id = ARGV[0].to_i
+
+puts post_id > 0 ? "Processing posts with IDs greater than ##{post_id}." : "No valid ID provided. Processing all posts."
+
+File.open('failed_iqdb_files.txt', 'a') do |log_file|
+  Post.all.where("id > ?", post_id ? post_id : 0).find_each do |post|
+    begin
+      IqdbClient.new.add_post(post)
+      puts "##{post.id} post iqdb regenerated"
+    rescue Errno::ENOENT => e
+      error_message = "Error: Unable to open original file for Post ##{post.id}: #{e.message}. Skipping post iqdb regeneration."
+      puts error_message
+      log_file.puts(error_message)
+    rescue StandardError => e
+      error_message = "An unexpected error occurred while processing Post ##{post.id}: #{e.message}. Skipping post iqdb regeneration."
+      puts error_message
+      log_file.puts(error_message)
+    end
+  end
+end
