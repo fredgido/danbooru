3b062a098955b3bd93d96b1d811446f94633bf2f
fredgido <fredgido@gmail.com>
2024-10-26 23:30:05 +0000

    migrations to keep konachan data

--- db/structure.sql
+++ db/structure.sql
@@ -652,7 +652,8 @@ ALTER SEQUENCE public.favorite_groups_id_seq OWNED BY public.favorite_groups.id;
 CREATE TABLE public.favorites (
     id integer NOT NULL,
     user_id integer NOT NULL,
-    post_id integer NOT NULL
+    post_id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 
@@ -1839,7 +1840,8 @@ CREATE TABLE public.saved_searches (
     query character varying NOT NULL,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
-    labels text[] DEFAULT '{}'::text[] NOT NULL
+    labels text[] DEFAULT '{}'::text[] NOT NULL,
+    is_visible_on_profile boolean DEFAULT false NOT NULL
 );
 
 
@@ -6905,6 +6907,8 @@ SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20241018065315'),
+('20241018035234'),
+('20241018034758'),
 ('20241026212428'),
 ('20241012193130'),
 ('20240926020203'),
