8896ae55c73737d95216754fa5c29097334bc3f4
fredgido <fredgido@gmail.com>
2024-10-27 15:00:34 +0000

    save media_metadata with same id as media_asset

--- db/migrate/20241026212429_add_legacy_score_to_post_votes.rb
+++ db/migrate/20241026212429_add_legacy_score_to_post_votes.rb
@@ -1,4 +1,4 @@
-class AddLegacyScoreToPostVotes < ActiveRecord::Migration[6.1]
+class AddLegacyScoreToPostVotes < ActiveRecord::Migration[7.1]
   def change
     add_column :post_votes, :legacy_score, :smallint, null: true
   end
